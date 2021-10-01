d2aedad8779ebb3e015eaed460c4894fb5c973f7
Felipe Delgado <af.delgador@uniandes.edu.co>
2021-10-01 14:24:28 -0500

    update

--- public/script.js
+++ public/script.js
@@ -5,10 +5,13 @@ const table = document.getElementById('table');
 const tbody = document.getElementById('body-table');
 const message = document.getElementById('message');
 
+
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+
+
+  getResults(input.value);
+  console.log(input.value);
   event.preventDefault();
 });
 
@@ -16,11 +19,13 @@ enterButton.addEventListener('click', (event) => {
  * Llamado al backend con queryParam
  * @param {*} heightRef
  */
-async function getresults(heightRef) {
+async function getResults(heightRef) {
+  console.log("-------")
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
+  console.log()
   console.log('data from back', data);
-  //printValues(data);
+  printValues(data);
 }
 
 function removeAllChildNodes(parent) {
@@ -28,3 +33,18 @@ function removeAllChildNodes(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+function printValues(data) {
+  const parejas = data.length;
+  for (let i = 0; i < parejas; i++) {
+    console.log(data[i]);
+    var row = tbody.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(1);
+    cell1.innerHTML = parejas - i;
+    cell2.innerHTML = `${data[i][0].first_name} ${data[i][0].last_name}`;
+    cell3.innerHTML = `${data[i][1].first_name} ${data[i][1].last_name}`;
+  }
+}
+
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,39 @@
 const { response, request } = require('express');
+const axios = require('axios');
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
+
+  let hashTwoSum = (array, sum) => {
+    let storageHash = {};
+    let couples = [];
+
+    for (let i in array) {
+      let addend = sum - array[i].h_in;
+
+      if (addend in storageHash) {
+        couples.push([storageHash[addend], array[i]]);
+      }
+      storageHash[array[i].h_in] = array[i];
+    }
+    return couples;
+  };
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  const input = req.query.input;
+  console.log(`input=${input}`);
+  try {
+    const response = await axios.get('https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players');
+    const players = response.data.values;
+    const result = hashTwoSum(players, input);
+    // const result = [[players[0], players[1]], [players[0], players[1]]];
+    resp.send(result)
+  }
+  catch (err) {
+    consoles.log(err)
+  }
 };
 
+
+
+
 module.exports = { getPairsOfPlayers };
+
